e83107384830cf34575d037e415a14b6ba040b5e
kianj-2005 <63022749+kianj-2005@users.noreply.github.com>
2020-10-18 22:28:06 +0100

    Add files via upload

---
+++ src/main/java/RandomItems/Main.java
@@ -0,0 +1,9 @@
+package RandomItems;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = Reference.MODID, name = Reference.MODNAME, version = Reference.VERSION)
+public class Main 
+{
+
+}
---
+++ src/main/java/RandomItems/Reference.java
@@ -0,0 +1,11 @@
+package RandomItems;
+
+public class Reference 
+{
+	public static final String MODID = "RandomItems";
+	public static final String MODNAME = "Random Items";
+	public static final String VERSION = "1.12.2";
+	
+	public static final String CLIENT = "RandomItems.tutorial.proxy.ClientProxy";
+	public static final String SERVER = "RandomItems.tutorial.proxy.CommonProxy";
+}
